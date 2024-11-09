cf09caf6d252add1960d9185ac9001492cc6d767
Farrukh Subhani <farrukh@bookingonline.co.uk>
2024-11-09 20:48:38 +0000

    Added SSL to vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    https: {
+      key: `./certs/localhost-key.pem`,
+      cert: `./certs/localhost.pem`,
     },
   },
 })
